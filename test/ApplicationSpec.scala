4c691cdc52c6a17ea05969fad2aeb7b1d1410342
Thibaut Géry <thibaut.gery@gmail.com>
2015-10-05 15:43:19 +0200

    add some simple integration test

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,6 +1,8 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
+import play.api.Configuration
+import play.api.inject.guice.GuiceApplicationBuilder
 
 import play.api.test._
 import play.api.test.Helpers._
@@ -13,18 +15,19 @@ import play.api.test.Helpers._
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
+  val app = new GuiceApplicationBuilder()
+    .configure(Configuration("mongodb.uri" -> "mongodb://localhost:27017/sample-test"))
+    .build()
+
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
+    "send 404 on a bad request" in new WithApplication(app) {
       route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
     }
 
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+    "send 200 on when getting all user" in new WithApplication(app) {
+      route(FakeRequest(GET, "/api/users")) must beSome.which (status(_) == OK)
     }
+
   }
 }
