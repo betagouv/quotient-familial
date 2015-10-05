7cfbc6f6b30099996a173f118ec0eb0025d85a5e
Thibaut Géry <thibaut.gery@gmail.com>
2015-10-05 11:20:18 +0200

    add simple json api

---
+++ app/models/User.scala
@@ -0,0 +1,25 @@
+
+
+package models
+
+import play.api.libs.json.{Reads, Writes}
+
+case class User(
+                 age: Int,
+                 firstName: String,
+                 lastName: String,
+                 feeds: List[Feed])
+
+case class Feed(
+                 name: String,
+                 url: String)
+
+object JsonFormats {
+  import play.api.libs.json.Json
+
+
+  // Generates Writes and Reads for Feed and User thanks to Json Macros
+  implicit val feedFormat = Json.format[Feed]
+  implicit val userFormat = Json.format[User]
+
+}
