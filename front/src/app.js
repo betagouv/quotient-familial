86b63fb74c8457de537013f10dcbb823c4bba556
Thibaut Géry <thibaut.gery@gmail.com>
2015-11-12 18:05:28 +0100

    use materialize for design purpose

--- front/src/app.js
+++ front/src/app.js
@@ -1,10 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
+
+//Material
+import 'angular-material/angular-material.css';
+// Animation
+import angularAnimate from 'angular-animate';
+// Materail Design lib
+import angularMaterial from 'angular-material';
 
 import routing from './app.config';
 import home from './features/home';
 
-angular.module('app', [uirouter, home])
+angular.module('app', [uirouter, home, angularMaterial])
   .config(routing);
--- front/src/features/home/home.routes.js
+++ front/src/features/home/home.routes.js
@@ -4,10 +4,6 @@ export default function routes($stateProvider) {
   $stateProvider
     .state('home', {
       url: '/',
-      template: require('./home.html')
-    })
-    .state('home.calculator', {
-      url: '/calculator',
       template: require('./calculator/calculator.html'),
       controller: 'CalculatorController',
       controllerAs: 'calculator'
