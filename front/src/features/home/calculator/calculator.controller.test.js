37adc324f42f54579a9bd46b50c9dba7ad4f1299
Thibaut Géry <thibaut.gery@gmail.com>
2015-11-27 15:23:57 +0100

    fix front test

--- front/src/features/home/calculator/calculator.controller.test.js
+++ front/src/features/home/calculator/calculator.controller.test.js
@@ -27,34 +27,44 @@ describe('Controller: Calculator', function() {
     describe('when clicking on calculate', () => {
       it('calculate the quotient famiale', () => {
         spyOn(SvairService, 'declaration').and.returnValue({
-        then(callback) {
-          callback({
-            revenuFiscalReference: 24000,
-            nombreParts: 2,
-            declarant1: {nom: 'toto'},
-            declarant2: {nom: 'tata'}
+          then(callback) {
+            callback({
+              revenuFiscalReference: 24000,
+              nombreParts: 2,
+              declarant1: {nom: 'toto'},
+              declarant2: {nom: 'tata'}
 
-          })
-        }});
+            })
+            return {
+              catch: function() {
+              }
+            }
+          }
+        });
         ctrl = $controller("CalculatorController");
         ctrl.getSvairInfo(1, 2);
         expect(ctrl.quotientFamilial).toEqual(1000)
         expect(ctrl.revenuFiscalReference).toEqual(24000)
         expect(ctrl.nombreParts).toEqual(2)
-        //expect(ctrl.declarant1).toEqual({nom: 'toto'})
-        //expect(ctrl.declarant2).toEqual({nom: 'tata'})
+        expect(ctrl.declarant1).toEqual({nom: 'toto'})
+        expect(ctrl.declarant2).toEqual({nom: 'tata'})
         expect(SvairService.declaration).toHaveBeenCalledWith(1, 2)
       })
 
       it('show the loader when the request is running', () => {
         spyOn(SvairService, 'declaration').and.returnValue({
-        then(callback) {
-          expect(ctrl.requestPending).toBe(true);
-          callback({
-            revenuFiscalReference: 24000,
-            nombreParts: 2
-          })
-        }});
+          then(callback) {
+            expect(ctrl.requestPending).toBe(true);
+            callback({
+              revenuFiscalReference: 24000,
+              nombreParts: 2
+            })
+            return {
+              catch: function() {
+              }
+            }
+          }
+        });
         ctrl.getSvairInfo(1, 2);
         expect(ctrl.requestPending).not.toBe(true);
       })
